82c36525cf8c3b27572f88cfd45cb5eb69d1d553
ZhidkovNA <115547518+ZhidkovNA@users.noreply.github.com>
2022-10-26 23:59:06 +0300

    Update task8.py
    Исправил ошибки:
    - Убрал цикл if - else
    - Вынес операции выполняющиеся 1 раз за пределы цикла ( 1 месяц, когда не учитывается ежемесячный рост цен)
    - Сократил код (a = a +1 эквивалентно a += 1)

--- Лабоаторная работа 3/task8.py
+++ Лабоаторная работа 3/task8.py
@@ -5,18 +5,16 @@ increase = 0.05
 
 month = 0  # количество месяцев, которое можно прожить
 
+money_capital += salary - spend
+spend += spend * 0.05
+month += + 1
+
 while money_capital > 0:
-    if month == 0:
-        money_capital = money_capital + salary - spend
-        spend = spend + spend * 0.05
-        month = month + 1
-    else:
-        money_capital = money_capital + salary - spend
-        spend = spend + spend * 0.05
-        month = month + 1
+    money_capital += salary - spend
+    spend += spend * 0.05
+    month += + 1
 
 print(money_capital)
-month = month - 1
+month -= 1
 
-# TODO Оформить решение
 print(month)
