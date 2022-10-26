4d395f29cf97650738dfc78124011b8ef87fd030
ZhidkovNA <115547518+ZhidkovNA@users.noreply.github.com>
2022-10-27 00:57:58 +0300

    Update task6.py
    Исправил код с учетом прохода в 1 раз и использованием функции (счетчика) enumerate

--- Лабоаторная работа 3/task6.py
+++ Лабоаторная работа 3/task6.py
@@ -1,9 +1,13 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO Оформить решение
-max_num = max(list_numbers)
-max_num_index = list_numbers.index(max_num)
-last_num_index = list_numbers.index(list_numbers[-1])
-list_numbers[max_num_index], list_numbers[last_num_index] = list_numbers[last_num_index], list_numbers[max_num_index]
+max_index = 0
+max_value = list_numbers[max_index]
+
+for i, value in enumerate(list_numbers):
+    if value > max_value:
+        max_value = value
+        max_index = i
+
+list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
 
 print(list_numbers)
